2e8d91ba7b3655b74d01003c8d3523eb7a4a7ad4
Mike Yeung <yeungmi3@oocl.com>
2025-09-04 17:36:32 +0800

    refractor: move try block out the else block

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -12,31 +12,30 @@ public class WordFrequencyGame {
         String[] words = inputStr.split(SPACE_SEPARATOR);
         if (words.length == 1) {
             return inputStr + " 1";
-        } else {
-            try {
-                List<Input> inputList = new ArrayList<>();
-                for (String s : words) {
-                    Input input = new Input(s, 1);
-                    inputList.add(input);
-                }
-                //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map = getListMap(inputList);
-                List<Input> list = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
-                    Input input = new Input(entry.getKey(), entry.getValue().size());
-                    list.add(input);
-                }
-                inputList = list;
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
-                StringJoiner joiner = new StringJoiner("\n");
-                for (Input w : inputList) {
-                    String s = w.getValue() + " " + w.getWordCount();
-                    joiner.add(s);
-                }
-                return joiner.toString();
-            } catch (Exception e) {
-                return "Calculate Error";
+        }
+        try {
+            List<Input> inputList = new ArrayList<>();
+            for (String s : words) {
+                Input input = new Input(s, 1);
+                inputList.add(input);
             }
+            //get the map for the next step of sizing the same word
+            Map<String, List<Input>> map = getListMap(inputList);
+            List<Input> list = new ArrayList<>();
+            for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
+                Input input = new Input(entry.getKey(), entry.getValue().size());
+                list.add(input);
+            }
+            inputList = list;
+            inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+            StringJoiner joiner = new StringJoiner("\n");
+            for (Input w : inputList) {
+                String s = w.getValue() + " " + w.getWordCount();
+                joiner.add(s);
+            }
+            return joiner.toString();
+        } catch (Exception e) {
+            return "Calculate Error";
         }
     }
 
